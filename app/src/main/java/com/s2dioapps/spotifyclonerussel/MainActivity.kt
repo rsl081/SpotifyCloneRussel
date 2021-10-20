34caaa4384733f69e5f873a8db6a2862edcf1601
rsl081 <53944109+rsl081@users.noreply.github.com>
2021-10-21 00:01:12 +0800

    rsl
    first init

---
+++ app/src/main/java/com/s2dioapps/spotifyclonerussel/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.s2dioapps.spotifyclonerussel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
