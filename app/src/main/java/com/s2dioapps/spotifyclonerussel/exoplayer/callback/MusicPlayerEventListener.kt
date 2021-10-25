89c3a61412e1929d20d30d71db6327e52b07da99
rsl081 <53944109+rsl081@users.noreply.github.com>
2021-10-25 17:43:48 +0800

    Resource & Event Class - MVVM Spotify Clone - Part 10

---
+++ app/src/main/java/com/s2dioapps/spotifyclonerussel/exoplayer/callback/MusicPlayerEventListener.kt
@@ -0,0 +1,25 @@
+package com.s2dioapps.spotifyclonerussel.exoplayer.callback
+
+import android.widget.Toast
+import com.google.android.exoplayer2.ExoPlaybackException
+import com.google.android.exoplayer2.PlaybackException
+import com.google.android.exoplayer2.Player
+import com.s2dioapps.spotifyclonerussel.exoplayer.MusicService
+
+class MusicPlayerEventListener(
+    private val musicService: MusicService
+) : Player.EventListener {
+
+    override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+        super.onPlayerStateChanged(playWhenReady, playbackState)
+        if(playbackState == Player.STATE_READY && !playWhenReady) {
+            musicService.stopForeground(false)
+        }
+    }
+
+    override fun onPlayerError(error: PlaybackException) {
+        super.onPlayerError(error)
+        Toast.makeText(musicService, "An unknown error occured", Toast.LENGTH_LONG).show()
+    }
+
+}
--- app/src/main/java/com/s2dioapps/spotifyclonerussel/other/Constants.kt
+++ app/src/main/java/com/s2dioapps/spotifyclonerussel/other/Constants.kt
@@ -2,4 +2,9 @@ package com.s2dioapps.spotifyclonerussel.other
 
 object Constants {
     const val SONG_COLLECTION = "songs"
+
+    const val MEDIA_ROOT_ID = "root_id"
+
+    const val NOTIFICATION_CHANNEL_ID = "music"
+    const val NOTIFICATION_ID = 1
 }
